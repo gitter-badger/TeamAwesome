af39fbd55f42608768ffe61b4b0f50ec58bb8f98
Justin Baumgartner <justbaum30@gmail.com>
2014-11-20 16:51:30 -0500

    Added keyboard view controller

---
+++ TeamAwesome/KeyboardViewController.m
@@ -0,0 +1,70 @@
+//
+//  KeyboardViewController.m
+//  TeamAwesome
+//
+//  Created by Justin Baumgartner  on 11/20/14.
+//  Copyright (c) 2014 Team Awesome. All rights reserved.
+//
+
+#import "KeyboardViewController.h"
+
+@interface KeyboardViewController ()
+
+@end
+
+@implementation KeyboardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (IBAction)cKeyTapped {
+    
+}
+
+- (IBAction)cSharpKeyTapped {
+    
+}
+
+- (IBAction)dKeyTapped {
+    
+}
+
+- (IBAction)dSharpKeyTapped {
+    
+}
+
+- (IBAction)eKeyTapped {
+    
+}
+
+- (IBAction)fKeyTapped {
+    
+}
+
+- (IBAction)fSharpKeyTapped {
+    
+}
+
+- (IBAction)gKeyTapped {
+    
+}
+
+- (IBAction)gSharpKeyTapped {
+    
+}
+
+- (IBAction)aKeyTapped {
+    
+}
+
+- (IBAction)aSharpKeyTapped {
+    
+}
+
+- (IBAction)bKeyTapped {
+    
+}
+
+@end
---
+++ TeamAwesome/PlaybackViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaybackViewController.h
+//  TeamAwesome
+//
+//  Created by Justin Baumgartner  on 11/20/14.
+//  Copyright (c) 2014 Team Awesome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaybackViewController : UIViewController
+
+@end

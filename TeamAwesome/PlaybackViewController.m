695d130a9930b006538570e629636420f59880ab
Justin Baumgartner <justbaum30@gmail.com>
2014-11-20 17:37:35 -0500

    Added music note model

--- TeamAwesome/PlaybackViewController.m
+++ TeamAwesome/PlaybackViewController.m
@@ -11,6 +11,10 @@
 
 @interface PlaybackViewController ()
 
+@property (assign, nonatomic) NSInteger notePosition;
+
+@property (weak, nonatomic) IBOutlet UILabel *notePositionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currentNoteLabel;
 @property (weak, nonatomic) IBOutlet UIView *keyboardView;
 
 @end
@@ -24,6 +28,23 @@
     [self addChildViewController:keyboardVC];
     [self.keyboardView addSubview:keyboardVC.view];
     [keyboardVC didMoveToParentViewController:self];
+}
+
+- (IBAction)notePositionStepperTapped:(UIStepper *)stepper {
+    self.notePosition = stepper.value;
+    self.notePositionLabel.text = [NSString stringWithFormat:@"Note Position: %ld/16", (long)self.notePosition];
+}
+
+- (IBAction)deleteNoteTapped:(id)sender {
+    
+}
+
+- (IBAction)playNoteTapped:(id)sender {
+    
+}
+
+- (IBAction)saveSongTapped:(id)sender {
+    
 }
 
 @end

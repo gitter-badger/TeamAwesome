da2488aca3bb84d85a15152df0d2f20fb2843800
Tom Wilk <tom.wilk@gmail.com>
2014-11-20 16:45:07 -0500

    Instrument class stub

---
+++ TeamAwesome/Instrument.h
@@ -0,0 +1,13 @@
+//
+//  Instrument.h
+//  TeamAwesome
+//
+//  Created by Tom Wilk on 11/20/14.
+//  Copyright (c) 2014 Team Awesome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Instrument : UIView
+
+@end
---
+++ TeamAwesome/Instrument.m
@@ -0,0 +1,21 @@
+//
+//  Instrument.m
+//  TeamAwesome
+//
+//  Created by Tom Wilk on 11/20/14.
+//  Copyright (c) 2014 Team Awesome. All rights reserved.
+//
+
+#import "Instrument.h"
+
+@implementation Instrument
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

ad21a9aa2cf39639da4980dc8aab0459454e1693
Tom Wilk <tom.wilk@gmail.com>
2014-11-20 17:10:29 -0500

    improvement to instrument sound

--- TeamAwesome/Instrument.h
+++ TeamAwesome/Instrument.h
@@ -7,7 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AKFoundation.h"
 
-@interface Instrument : UIView
+
+
+@interface Instrument : AKInstrument
+
+
+@property (nonatomic, strong) AKInstrumentProperty *frequencyValue;
+@property (nonatomic, strong) AKInstrumentProperty *carrierMultValue;
+@property (nonatomic, strong) AKInstrumentProperty *modIndexValue;
+@property (nonatomic, strong) AKInstrumentProperty *amplitudeValue;
+@property (nonatomic, assign) float attackValue;
+@property (nonatomic, assign) float decayValue;
+@property (nonatomic, assign) float sustainValue;
+@property (nonatomic, assign) float releaseValue;
+
+
+-(void)loadInstrument:(NSDictionary *)instrumentDef;
+-(void)saveInstrument:(NSDictionary *)instrumentDef;
+-(void)playNote:(AKInstrumentProperty* )freq;
+-(void)silence;
 
 @end
--- TeamAwesome/Instrument.m
+++ TeamAwesome/Instrument.m
@@ -10,12 +10,100 @@
 
 @implementation Instrument
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+{
+    AKFMOscillator *osc;
+    AKLinearADSRAudioEnvelope *env;
 }
-*/
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+        // INPUTS AND CONTROLS =================================================
+        _frequencyValue = [[AKInstrumentProperty alloc] initWithValue:440
+                                                              minimum:220
+                                                              maximum:880];
+        
+        _carrierMultValue   = [[AKInstrumentProperty alloc] initWithValue:0.5
+                                                                  minimum:0
+                                                                  maximum:1];
+        
+        _modIndexValue   = [[AKInstrumentProperty alloc] initWithValue:0.5
+                                                               minimum:0
+                                                               maximum:1];
+        
+        _amplitudeValue   = [[AKInstrumentProperty alloc] initWithValue:0.5
+                                                                minimum:0
+                                                                maximum:0.5];
+        
+        [self addProperty:_frequencyValue];
+        [self addProperty:_carrierMultValue];
+        [self addProperty:_modIndexValue];
+        [self addProperty:_amplitudeValue];
+        _attackValue = 0.1;
+        _decayValue = 0.5;
+        _sustainValue  = 0.1;
+        _releaseValue = 0.5;
+        
+    
+        
+        AKSineTable *sineTable = [[AKSineTable alloc] init];
+        [self addFTable:sineTable];
+        
+        AKLinearADSRAudioEnvelope *envelope = [[AKLinearADSRAudioEnvelope alloc]
+                                               initWithAttackDuration:akp(_attackValue)
+                                               decayDuration:akp(_decayValue)
+                                               sustainLevel:akp(_sustainValue)
+                                               releaseDuration:akp(_releaseValue)];
+        
+       
+        [self connect:envelope];
+        
+        AKFMOscillator *fmOscillatorlator;
+        fmOscillatorlator = [[AKFMOscillator alloc] initWithFTable:sineTable
+                                                     baseFrequency:_frequencyValue
+                                                 carrierMultiplier:_carrierMultValue
+                                              modulatingMultiplier:akp(0.5)
+                                                   modulationIndex:_modIndexValue
+                                                         amplitude:envelope];
+        [self connect:fmOscillatorlator];
+        
+        
+        env = envelope;
+        osc = fmOscillatorlator;
+        
+        // AUDIO OUTPUT ========================================================
+        
+        AKAudioOutput *audioOutput = [[AKAudioOutput alloc] initWithAudioSource:fmOscillatorlator];
+        [self connect:audioOutput];
+        
+        
+    }
+    return self;
+}
+
+-(void)loadInstrument:(NSDictionary *)instrumentDef
+{
+   
+    
+}
+
+-(void)saveInstrument:(NSDictionary *)instrumentDef
+{
+    
+}
+-(void)playNote:(AKInstrumentProperty*)freq
+{
+    AKInstrumentProperty *p = (AKInstrumentProperty *)_frequencyValue;
+    p = freq;
+    [self play];
+}
+-(void)silence
+{
+    [self stop];
+}
+
+
 
 @end
